a18bac5b156816607c475afcc29842700fc3c414
LuminolT <copper_sulfate@qq.com>
2022-05-23 23:35:40 +0800

    1

---
+++ include/MySort.h
@@ -0,0 +1,19 @@
+/**
+ * @file MySort.h
+ * @Author : Luminolt (copper_sulfate@qq.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-05-23
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef INCLUDE_MY_SORT_H
+#define INCLUDE_MY_SORT_H
+
+class MySort {
+    public:
+}
+
+#endif // INCLUDE_MY_SORT_H
